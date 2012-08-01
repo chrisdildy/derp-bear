4ab4284ed4fe2c4518784451225252d704809271
Dildy <iwasdildy@Chris-Dildys-iMac.local>
2012-07-31 23:33:09 -0400

    test array
    test array.

---
+++ Milkshake04.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+
+public class Milkshake04
+{
+ public static void main(String[] args)
+ {
+ 		 String[] a = new String[] {"Banana", "Pineapple", "Cherry", "Peach",
+ 		 		 "Blueberry", "Strawberry", "BerryBerry", "Mocha", "Oreo",
+ 		 		 "Mint", "Cobbler", "Chocolate", "Vanilla", "Fudge", "HiCPunch",
+ 		 		 "Snickers", "Reeses", "Walnut", "Chocolate Malt", "Cappuccino",
+ 		 		 "Caramel", "Peanut Butter", "Cheesecake", "Brownie", 
+ 		 "Heath Toffee", "Orange Push Up", "Egg Nog", "Watermelon"};
+ 		 
+ 		 System.out.println(a[0]);
+ 		 System.out.println(a[1]);
+ 		 System.out.println(a[12]);
+ }
+}
